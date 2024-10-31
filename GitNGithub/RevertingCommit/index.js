07cb854a8d65f7cdeb93fa9f81279d89a1acc698
Samir Khanal <samirkhanaal9@gmail.com>
2024-10-31 17:32:26 +0530

    Changes Done

---
+++ GitNGithub/RevertingCommit/index.js
@@ -0,0 +1,12 @@
+const name1="Samir Khanal"
+
+function sayName(){
+  console.log("My name is "+name1)
+}
+
+sayName()
+
+let name2="samir"
+
+let name3="khanal"
+
---
+++ GitNGithub/WhatisVCS/InitializeGit/index.js
@@ -0,0 +1,6 @@
+const age=24
+
+const name="Piyush Garg"
+
+
+
